def10d97edabffd66dfc0876ab335d96e8414175
JuanchiTaveira <juantaveira10@gmail.com>
2023-08-13 15:21:37 +0200

    center text and change font colors

--- src/main/java/com/example/iaimagemaker/services/ImageTextService.java
+++ src/main/java/com/example/iaimagemaker/services/ImageTextService.java
@@ -1,12 +1,12 @@
 package com.example.iaimagemaker.services;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import javax.imageio.ImageIO;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +17,27 @@ public class ImageTextService {
         URL url = new URL(imageUrl);
         BufferedImage image = ImageIO.read(url);
 
-        Graphics2D g = image.createGraphics();
-        Font font = new Font("Arial", Font.BOLD, 24);
+        Color backgroundColor = getAverageBackgroundColor(image);
+        Color textColor = getContrastingTextColor(backgroundColor);
 
-        g.setFont(font);
-        g.setColor(Color.WHITE);
+        Graphics2D graphics = image.createGraphics();
 
-        int x = 50;
-        int y = 50;
+        Font font = new Font("Arial", Font.BOLD, 60);
 
-        g.drawString(text, x, y);
-        g.dispose();
+        graphics.setFont(font);
+        graphics.setColor(textColor);
+
+        String[] lines = divideTextIntoLines(text, graphics.getFontMetrics(), image.getWidth());
+
+        int y = (image.getHeight() - (lines.length * graphics.getFontMetrics().getHeight())) / 2;
+
+        for (String line : lines) {
+            int x = (image.getWidth() - graphics.getFontMetrics().stringWidth(line)) / 2;
+            graphics.drawString(line, x, y);
+            y += graphics.getFontMetrics().getHeight();
+        }
+
+        graphics.dispose();
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "jpg", baos);
@@ -36,5 +46,74 @@ public class ImageTextService {
         baos.close();
 
         return imageBytes;
+    }
+
+    private static String[] divideTextIntoLines(String text, FontMetrics fontMetrics, int maxWidth) {
+        List<String> lines = new ArrayList<>();
+        String[] words = text.split(" ");
+        StringBuilder currentLine = new StringBuilder(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i];
+            String potentialLine = currentLine + " " + word;
+
+            if (fontMetrics.stringWidth(potentialLine) <= maxWidth) {
+                currentLine.append(" ").append(word);
+            } else {
+                lines.add(currentLine.toString());
+                currentLine = new StringBuilder(word);
+            }
+        }
+
+        lines.add(currentLine.toString());
+
+        return lines.toArray(new String[0]);
+    }
+
+    private static Color getContrastingTextColor(Color backgroundColor) {
+        int luminance = (int) ((0.299 * backgroundColor.getRed()) + (0.587 * backgroundColor.getGreen()) + (0.114 * backgroundColor.getBlue()));
+
+        if (luminance > 128) {
+            Color[] darkColors = {
+                    new Color(31, 38, 42),
+                    new Color(63, 81, 181),
+                    new Color(48, 63, 159),
+                    new Color(149, 117, 205),
+                    new Color(194, 24, 91)
+            };
+            return darkColors[(int) (Math.random() * darkColors.length)];
+        } else {
+            Color[] lightColors = {
+                    new Color(239, 247, 255),
+                    new Color(255, 193, 7),
+                    new Color(156, 204, 101),
+                    new Color(255, 138, 101),
+                    new Color(244, 67, 54)
+            };
+            return lightColors[(int) (Math.random() * lightColors.length)];
+        }
+    }
+
+    private static Color getAverageBackgroundColor(BufferedImage image) {
+        int totalRed = 0;
+        int totalGreen = 0;
+        int totalBlue = 0;
+        int pixelCount = 0;
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                Color pixelColor = new Color(image.getRGB(x, y));
+                totalRed += pixelColor.getRed();
+                totalGreen += pixelColor.getGreen();
+                totalBlue += pixelColor.getBlue();
+                pixelCount++;
+            }
+        }
+
+        int avgRed = totalRed / pixelCount;
+        int avgGreen = totalGreen / pixelCount;
+        int avgBlue = totalBlue / pixelCount;
+
+        return new Color(avgRed, avgGreen, avgBlue);
     }
 }
